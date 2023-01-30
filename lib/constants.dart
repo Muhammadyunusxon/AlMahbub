086e160255cd22bf066b9e0b2497ae994cde3f6c
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-01-30 15:53:28 +0500

    add images and fixed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-const kSecondaryColor = Color(0xFFFE6D8E);
-const kTextColor = Color(0xFF12153D);
-const kTextLightColor = Color(0xFF9A9BB2);
 const kBrandColor = Color(0xFFFBBA00);
 const kTextDarkColor = Color(0xff01000D);
 const kGreenColor = Color(0xff0F6657);
 const kYellowColor = Color(0xffFBA808);
+const kWhiteColor = Color(0xffffffff);
+const kTextGreenColor = Color(0xff00AD8F);
+const kBGColor = Color(0xffF8F9FA);
 
 const kDefaultPadding = 20.0;
 
